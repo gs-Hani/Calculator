ff4ee7e883cf121f44bf0fd0d8ef09980cc6a25b
gs-Hani <hani_alazawi@hotmail.com>
2024-04-09 11:37:52 +0400

    first commit: sketch structure

---
+++ claculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>;
+
+int main () {
+
+	int n1,n2,result;
+	char operation;
+
+	printf("First Number:");
+	scanf("%i",&n1);
+
+	printf("Operation:");
+	scanf("%c",&operation);
+
+	printf("Second Number:");
+	scanf("%i",&n2);
+	 
+	return 0;
+}
